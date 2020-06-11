a444a44cf74344acdaa0cbefc2c481d25834d6b3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-11 10:14:24 +0200

    Updated comments in issue.py

--- ci/issue.py
+++ ci/issue.py
@@ -11,6 +11,8 @@ from argparse import ArgumentParser
 
 def current_time_formatted():
     """Prepare timestamp in a format: '2020-03-10T16:00:00Z' that is accepted by GitHub API."""
+    # Retrieve current time represented in UTC time zone and format it
+    # accordingly to human readable format.
     return datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
 
@@ -18,25 +20,27 @@ def make_github_issue(title, body=None, created_at=None, closed_at=None, updated
                       assignee=None, milestone=None, closed=None, labels=None, token=None,
                       organization=None, repository=None):
     """Create an issue on github.com using the given parameters."""
-    # Url to create issues via POST
+    # URL to create issues via `POST` HTTP method.
     url = 'https://api.github.com/repos/%s/%s/import/issues' % (organization, repository)
 
-    # Headers
+    # Construct headers that need to be set.
     headers = {
         "Authorization": "token %s" % token,
         "Accept": "application/vnd.github.golden-comet-preview+json"
     }
 
-    # Create our issue
+    # Create data structure representing the issue.
     data = {'issue': {'title': title,
                       'body': body,
                       'created_at': created_at,
                       'assignee': assignee}}
 
+    # The data structure needs to be converted (marshalled) into JSON format.
     payload = json.dumps(data)
 
-    # Add the issue to our repository
+    # Add the issue to the specified repository by using REST API method.
     response = requests.request("POST", url, data=payload, headers=headers)
+    # Check if the issue has been created or if any error happens.
     if response.status_code == 202:
         print('Successfully created Issue "%s"' % title)
     else:
@@ -46,21 +50,23 @@ def make_github_issue(title, body=None, created_at=None, closed_at=None, updated
 
 def cli_arguments():
     """Retrieve all CLI arguments."""
+    # First of all, we need to specify all command line flags that are
+    # recognized by this tool.
     parser = ArgumentParser()
 
     # Authentication for user filing issue (must have read/write access to
-    # repository to add issue to)
+    # repository to add issue to).
     parser.add_argument("-t", "--token", dest="token", help="authentication token",
                         action="store", default=None, type=str, required=True)
 
-    # The repository to add this issue to
+    # The repository to add this issue to.
     parser.add_argument("-o", "--organization", dest="organization",
                         help="organization or repository owner",
                         action="store", default=None, type=str, required=True)
     parser.add_argument("-r", "--repository", dest="repository", help="repository name",
                         action="store", default=None, type=str, required=True)
 
-    # Issue-related options
+    # Issue-related options.
     parser.add_argument("-i", "--title", dest="title", help="issue title",
                         action="store", default=None, type=str, required=True)
 
@@ -70,10 +76,12 @@ def cli_arguments():
     parser.add_argument("-a", "--assignee", dest="assignee", help="default assignee",
                         action="store", default=None, type=str, required=True)
 
-    # Other options
+    # Other options.
     parser.add_argument("-v", "--verbose", dest="verbose", help="make operations verbose",
                         action="store_true", default=None)
 
+    # Now it is time to parse flags, check the actual content of command line
+    # and fill in the object named `args`.
     return parser.parse_args()
 
 
@@ -85,5 +93,7 @@ def main():
                       organization=args.organization, repository=args.repository, token=args.token)
 
 
+# If this script is started from command line, run the `main` function which is
+# entry point to the processing.
 if __name__ == "__main__":
     main()
