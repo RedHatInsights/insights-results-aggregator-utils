5b45c268d28b2d516252735b0ff9e1f2193f52ba
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-02 08:00:44 +0200

    Added link to generated documentation

--- reports/affected_clusters.py
+++ reports/affected_clusters.py
@@ -34,6 +34,9 @@ ccx-data-pipeline-qa/browse/secrets/ccx-data-pipeline-db
 ccx-data-pipeline-prod/browse/secrets/ccx-data-pipeline-db
 """
 
+# Link to generated documentation for this script:
+# https://redhatinsights.github.io/insights-results-aggregator-utils/packages/affected_clusters.html
+
 import json
 import sys
 import csv
