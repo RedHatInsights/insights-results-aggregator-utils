a6a4c55d89644623a627488f1359d6031184c48b
Juan Diaz Suarez <jdiazsua@redhat.com>
2021-09-27 08:22:25 +0200

    added a function to create a TLS configuration

---
+++ kafka/tlsconfig.go
@@ -0,0 +1,33 @@
+package kafka
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+// NewTLSConfig create a TLS configuration from a certificate path. This can be
+// used with Sarama for example.
+func NewTLSConfig(certPath string) (*tls.Config, error) {
+	if certPath == "" {
+		return nil, fmt.Errorf("No cert path provided. Skip")
+	}
+	tlsConfig := tls.Config{
+		Certificates: []tls.Certificate{},
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(filepath.Clean(certPath))
+	if err != nil {
+		return nil, err
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	tlsConfig.RootCAs = caCertPool
+
+	tlsConfig.BuildNameToCertificate()
+	return &tlsConfig, err
+}
