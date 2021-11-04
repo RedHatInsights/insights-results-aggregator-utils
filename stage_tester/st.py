75ae3c85508f507f517ab8eee4ba43e11d084bc5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-04 07:48:08 +0100

    Stub for the script

---
+++ stage_tester/st.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Copyright © 2021 Pavel Tisnovsky
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Script to retrieve results from the external data pipeline through the standard REST API.
+
+Description
+-----
+
+Usage
+-----
+
+
+
+Generated documentation
+-----
+<https://redhatinsights.github.io/insights-results-aggregator-utils/packages/st.html>
+"""
+
+def main():
+    """Entry point to this script."""
+
+
+# If this script is started from command line, run the `main` function which is
+# entry point to the processing.
+if __name__ == "__main__":
+    main()
