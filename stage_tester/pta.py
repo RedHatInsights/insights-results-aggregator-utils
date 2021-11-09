15d952ae02e50717a415ac5c83249637dbb337d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-09 14:58:15 +0100

    Stub for the tool

---
+++ stage_tester/pta.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Copyright © 2021 Pavel Tisnovsky
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Script to retrieve and analyze processing times from reports taken from external data pipeline.
+
+Description
+-----
+
+Usage
+-----
+```
+```
+
+Examples
+-----
+
+
+Generated documentation in literate programming style
+-----
+<https://redhatinsights.github.io/insights-results-aggregator-utils/packages/pta.html>
+"""
+
+import json
+from argparse import ArgumentParser
+
+
+def cli_arguments():
+    """Retrieve all CLI arguments provided by user."""
+    # First of all, we need to specify all command line flags that are
+    # recognized by this tool.
+    parser = ArgumentParser()
+    pass
