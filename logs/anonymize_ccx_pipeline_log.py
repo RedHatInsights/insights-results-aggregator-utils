3db6f7d28672de0a2ed9729204a0bc8e6952a5a9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-06 07:58:19 +0200

    Reformatted by Black

--- logs/anonymize_ccx_pipeline_log.py
+++ logs/anonymize_ccx_pipeline_log.py
@@ -50,16 +50,16 @@ def split_by_two_strings(line, str1, str2):
 def hash_org_id(line, salt):
     """Hash organization ID and return new line with encrypted value."""
     # First we need to retrieve the organization ID from input line.
-    beginning, org_id, ending = split_by_two_strings(line,
-                                                     'Message context: OrgId=',
-                                                     ', ClusterName="')
+    beginning, org_id, ending = split_by_two_strings(
+        line, "Message context: OrgId=", ', ClusterName="'
+    )
 
     # Initialize hashing algorithm. Hash to 8 bytes might be enough for
     # organization ID (`long int` originally).
     h = blake2b(digest_size=8, salt=salt)
 
     # Convert string with organization ID to bytes and perform the hashing.
-    h.update(org_id.encode('utf-8'))
+    h.update(org_id.encode("utf-8"))
 
     # Now it is possible to retrieve the hash in hex format and convert it
     # back to integer.
@@ -72,47 +72,48 @@ def hash_org_id(line, salt):
 def hash_cluster_id(line, salt):
     """Hash cluster ID and return new line with encrypted value."""
     # First we need to retrieve the cluster ID from input line.
-    beginning, cluster_id, ending = split_by_two_strings(line,
-                                                         ' ClusterName="',
-                                                         '", LastChecked="')
+    beginning, cluster_id, ending = split_by_two_strings(
+        line, ' ClusterName="', '", LastChecked="'
+    )
 
     # Initialize hashing algorithm. Hash to 16 bytes is enough for
     # cluster ID (32 hexa characters).
     h = blake2b(digest_size=16, salt=salt)
 
     # Convert string with cluster ID to bytes and perform the hashing.
-    h.update(cluster_id.encode('utf-8'))
+    h.update(cluster_id.encode("utf-8"))
 
     # Now it is possible to retrieve the hash in hex format.
     x = h.hexdigest()
 
     # Format all parts of log entry into expected output.
-    return "{}{}-{}-{}-{}-{}{}".format(beginning, x[0:8], x[8:12], x[12:16],
-                                       x[16:20], x[20:], ending)
+    return "{}{}-{}-{}-{}-{}{}".format(
+        beginning, x[0:8], x[8:12], x[12:16], x[16:20], x[20:], ending
+    )
 
 
-def hash_sensitive_values(line, salt=b'foo'):
+def hash_sensitive_values(line, salt=b"foo"):
     """Hash all sensitive values on line."""
     return hash_cluster_id(hash_org_id(line, salt), salt)
 
 
-def hash_url(line, salt=b'foo'):
+def hash_url(line, salt=b"foo"):
     """Change URL part from log line (not relevant anyway)."""
     # Retrieve the URL part from log line
-    url = match(r'.* (https://\S+)', line).groups()[0]
+    url = match(r".* (https://\S+)", line).groups()[0]
 
     # Initialize hashing algorithm. Hash to 32 bytes is enough for
     # unique URL
     h = blake2b(digest_size=32, salt=salt)
 
     # Convert string with URL to bytes and perform the hashing.
-    h.update(url.encode('utf-8'))
+    h.update(url.encode("utf-8"))
 
     # Now it is possible to retrieve the hash in hex format.
     x = h.hexdigest()
 
     # And use the hash instead of original URL.
-    return sub(r'https://\S+', "https://example.com/" + x, line)
+    return sub(r"https://\S+", "https://example.com/" + x, line)
 
 
 def anonymize_payload_hash(line):
@@ -120,7 +121,7 @@ def anonymize_payload_hash(line):
     # Payload Tracker uses an unique string which is 32 characters long
     # and contains only alphanumeric characters. It is easy to replace such
     # string with its anonymized version (it has no meaning for log processing).
-    return sub(r'[a-z0-9]{32}', '{anonymized}', line)
+    return sub(r"[a-z0-9]{32}", "{anonymized}", line)
 
 
 def main():
@@ -128,16 +129,22 @@ def main():
     # First of all, we need to specify all command line flags that are
     # recognized by this tool.
     parser = ArgumentParser()
-    parser.add_argument("-s", "--salt", dest="salt",
-                        help="salt for hashing algorithm",
-                        action="store", default="foo", required=True)
+    parser.add_argument(
+        "-s",
+        "--salt",
+        dest="salt",
+        help="salt for hashing algorithm",
+        action="store",
+        default="foo",
+        required=True,
+    )
 
     # Now it is time to parse flags, check the actual content of command line
     # and fill in the object named `args`.
     args = parser.parse_args()
 
     # Retrieve the salt to be used by hashing algorithm.
-    salt = args.salt.encode('utf-8')
+    salt = args.salt.encode("utf-8")
 
     # This script works as a standard Unix filter (input->output), so we have
     # to process input in line-by-line basis.
@@ -145,7 +152,7 @@ def main():
         line = line.strip()
         # If the line contains any information that need to be anonymized,
         # do so.
-        if 'Message context: OrgId=' in line and ', ClusterName="' in line:
+        if "Message context: OrgId=" in line and ', ClusterName="' in line:
             line = hash_sensitive_values(line, salt=salt)
         if "https://" in line:
             line = hash_url(line, salt=salt)
