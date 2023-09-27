96cf25cdec7635c789182f4462d70b4e386a57b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-09-27 10:41:56 +0200

    Added missing docstrings

--- reports/plot_write_duration.py
+++ reports/plot_write_duration.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Plot write into database durations measured and stored in CSV file."""
+
 import sys
 import csv
 import numpy as np
